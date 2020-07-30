e6bfb80b9ba5d050890d336f1156d66a70426b7c
sixlty <593469005@qq.com>
2020-07-30 14:53:12 +0800

    login

---
+++ src/global/regxp.js
@@ -0,0 +1,2 @@
+export const userNameExp = /[A-Za-z0-9_-]/g
+export const passwordExp = /[A-Za-z0-9]{6,18}/
--- src/pages/login/views/index.vue
+++ src/pages/login/views/index.vue
@@ -1,23 +1,67 @@
 <template>
   <div class="login-container">
     <div class="login-box">
-      <h1><img class="logo" :src="src">Vue admin</h1>
+      <h1>Vue admin</h1>
       <div class="form">
         <div class="item">
-          <span class="item-icon el-icon-user-solid"></span>
-          <input class="item-content" type="text">
+          <div class="content" :class="{'has-error': isUserNameError, 'active': !isUserNameError}">
+            <span class="item-icon el-icon-user-solid"></span>
+            <input v-model="form.userName" placeholder="请输入账号" class="item-content" type="text" @change="userNameChange">
+          </div>
+          <div v-if="isUserNameError" class="error-tip">{{ userNameErrorMsg }}</div>
+        </div>
+        <div class="item">
+          <div class="content" :class="{'has-error': isPasswordError, 'active': !isPasswordError}">
+            <span class="item-icon el-icon-unlock"></span>
+            <input v-model="form.password" placeholder="请输入密码" class="item-content" type="password" @change="passwordChange">
+          </div>
+          <div v-if="isPasswordError" class="error-tip">{{ passwordErrorMsg }}</div>
+        </div>
+        <div class="btn">
+          <button class="login-btn">登 录</button>
         </div>
       </div>
     </div>
-    <div class="footer">2</div>
+    <div class="footer">
+      xxx出品
+    </div>
   </div>
 </template>
 
 <script>
+import { userNameExp, passwordExp } from '@/global/regxp'
 export default {
   data() {
     return {
-      src: require('@/assets/logo.png')
+      src: require('@/assets/logo.png'),
+
+      isUserNameError: false,
+      userNameErrorMsg: '',
+      isPasswordError: false,
+      passwordErrorMsg: '',
+
+      form: {
+        userName: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    userNameChange(val) {
+      if(!userNameExp.test(val.target.value)) {
+        this.isUserNameError = true
+        this.userNameErrorMsg = '请输入正确格式的账号'
+      } else {
+        this.isUserNameError = false
+      }
+    },
+    passwordChange(val) {
+      if(!passwordExp.test(val.target.value)) {
+        this.isPasswordError = true
+        this.passwordErrorMsg = '密码格式不正确'
+      } else {
+        this.isPasswordError = false
+      }
     }
   }
 }
@@ -37,13 +81,14 @@ export default {
 
   .login-box {
     margin-top: 20vh;
-    line-height: 50px;
+    line-height: 40px;
 
     h1 {
       text-align: center;
     }
 
     .logo {
+      width: 45px;
       margin-right: 15px;
       vertical-align: middle;
     }
@@ -52,35 +97,89 @@ export default {
       display: flex;
       flex-direction: column;
       align-items: center;
+      margin-top: 20px;
 
       .item {
-        border: 2px solid rgb(139, 141, 137);
-        border-radius: 10px;
-        font-size: 20px;
         width: 400px;
+        padding-bottom: 25px;
+        margin-bottom: 10px;
+        height: 64px;
 
-        .item-icon {
-          width: 40px;
+        .content {
+          border: 2px solid rgb(139, 141, 137);
+          background-color:rgb(31, 30, 30);
+          border-radius: 10px;
           height: 40px;
-          text-align: center;
+          line-height: 32px;
+          font-size: 20px;
+          .item-icon {
+            width: 40px;
+            height: 25px;
+            text-align: center;
+            display: inline-block;
+          }
+
+          .item-content {
+            display: inline-block;
+            font-size: 15px;
+            font-family: -apple-system,BlinkMacSystemFont,Segoe UI,PingFang SC,Hiragino Sans GB,Microsoft YaHei,Helvetica Neue,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+            color: rgb(163, 162, 162);
+            width: 350px;
+            height: 25px;
+            line-height: 25px;
+            border: none;
+            padding: 0;
+            vertical-align: middle;
+            background-color:rgb(31, 30, 30);
+          }
+          .item-content:focus {
+            outline-color: rgb(31, 30, 30);
+          }
+        }
+        .has-error {
+          border-color:rgb(201, 35, 35);
+        }
+      }
+
+      .active:hover, .active:focus {
+        border-color: rgb(60, 150, 185);
+      }
+
+      .btn {
+        margin-top: 40px;
+        .login-btn {
           display: inline-block;
+          width: 400px;
+          background-color: rgb(60, 150, 185);
+          border: none;
+          border-radius: 5px;
+          padding: 10px 10px;
+          font-size: 15px;
+          cursor: pointer;
         }
 
-        .item-content {
-          display: inline-block;
-          color: gray;
-          width: 350px;
-          height: 36px;
-          border: none;
-          background: #000;
-          padding: 0;
-          vertical-align: middle
+        .login-btn:focus {
+          outline: none;
         }
-        .item-content:focus {
-          border: none;
+        .login-btn:hover {
+          background-color: rgb(67, 173, 214);
         }
       }
     }
+    .error-tip {
+      line-height: 20px;
+      font-size: 14px;
+      width: 400px;
+      color: rgb(168, 40, 40);
+    }
+
+  }
+
+  .footer {
+    position: fixed;
+    bottom: 20px;
+    font-family: -apple-system,BlinkMacSystemFont,Segoe UI,PingFang SC,Hiragino Sans GB,Microsoft YaHei,Helvetica Neue,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+    color:rgb(139, 141, 137);
   }
 }
 </style>
